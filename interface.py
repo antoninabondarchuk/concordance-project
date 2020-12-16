95a11c2ce8459b0c6a7e8a12b9e416179e62afe2
Antonina Bondarchuk <antonina.bondarchuk@nure.ua>
2020-12-16 15:56:44 +0200

    Create UI for inputs

--- interface.py
+++ interface.py
@@ -38,8 +38,30 @@ class Root(Tk):
                                style='W.TLabelframe.Label')
         self.label.grid(column=1, row=2)
         self.label.configure(text=f'From: {self.filename}')
+
         self.filename = get_raw_path(self.filename)
         self.files_list = read_all_txt_from_dir(self.filename)
+
+        self.word_label = ttk.Label(text="Please, input words: ",
+                                    style='W.TLabelframe.Label')
+        self.word_label.grid(column=0, row=3)
+
+        self.entry_word = ttk.Entry(self)
+        self.entry_word.config(width=50)
+        self.entry_word.grid(column=0, row=4)
+        self.entry_button = ttk.Button(self,
+                                       text="Press to display result",
+                                       command=self.display_result)
+        self.entry_button.grid(column=1, row=5)
+
+        self.window_text = ttk.Label(
+            text="Please, choose context width:")
+        self.window_text.grid(column=1, row=3)
+        self.window_spin = ttk.Spinbox(self, from_=25, to=200, width=5)
+        self.window_spin.grid(column=1, row=4)
+
+    def display_result(self):
+        self.entered_str = self.entry_word.get()
 
 
 # Starting the application by drawing the new window
