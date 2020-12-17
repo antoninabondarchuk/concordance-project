68a6c2f4e599033a67774acd99d8f710428f59dc
Antonina Bondarchuk <antonina.bondarchuk@nure.ua>
2020-12-17 20:17:33 +0200

    Add word forms functionality and font colors

--- interface.py
+++ interface.py
@@ -7,6 +7,11 @@ from preparation import get_raw_path, read_all_txt_from_dir
 from customtext import CustomText
 
 
+INVALID_WORD_MESSAGE = 'Please, input correct word!'
+NO_FILE_RESULTS_MESSAGE = 'NO MATCHES \n\n\n\n'
+WORDS_COLORS = ("Pink", "Blue", "Green")
+
+
 class Root(Tk):
     """
     A class for new window painting.
@@ -53,17 +58,13 @@ class Root(Tk):
         self.entry_button = ttk.Button(self,
                                        text="Press to display result",
                                        command=self.display_result)
-        self.entry_button.grid(column=1, row=5)
-
-        self.window_text = ttk.Label(
-            text="Please, choose context width:")
-        self.window_text.grid(column=1, row=3)
-        self.window_spin = ttk.Spinbox(self, from_=25, to=200, width=5)
-        self.window_spin.grid(column=1, row=4)
+        self.entry_button.grid(column=0, row=5)
 
     def display_result(self):
         self.result_text = CustomText(self)
-        self.result_text.tag_configure("Pink", foreground="#ff087f")
+        self.result_text.tag_configure(WORDS_COLORS[0], foreground="#ff087f")
+        self.result_text.tag_configure(WORDS_COLORS[1], foreground="blue")
+        self.result_text.tag_configure(WORDS_COLORS[2], foreground="green")
         self.result_text.config(width=90, height=30)
         self.result_text.grid(column=0, row=6, columnspan=2,
                               padx=20, pady=20)
@@ -75,27 +76,39 @@ class Root(Tk):
 
         self.result_text.configure(yscrollcommand=self.text_scroll.set)
 
-        entered_str = self.entry_word.get().strip()
-        entered_word = (entered_str[:-1]
-                        if entered_str[-1] == '*' else entered_str)
-
-        for file in self.files_list:
-            self.result_text.insert(END, file + '\n' * 3)
-
-            with open(file, encoding="utf8") as f:
-                line_num = 1
-                for line in f:
-                    line_result = f'Line #{line_num}: {line}' + '\n' * 2
-                    self.result_text.insert(END, line_result)
-                    line_num += 1
-
-        REGEX = r'(?i) {}'.format(entered_word)
-        if entered_str[-1] == '*':
-            REGEX += r'\w*'
+        entered_str_list = self.entry_word.get().strip().split()
+        if not entered_str_list:
+            # displaying corresponding message if input is empty
+            self.result_text.insert(END, INVALID_WORD_MESSAGE)
         else:
-            REGEX += ' '
+            entered_words_list = [' ' + entered_str[:-1].lower()
+                if entered_str[-1] == '*' else ' ' + entered_str.lower()
+                for entered_str in entered_str_list]
 
-        self.result_text.highlight_pattern(REGEX, "Pink")
+            for file in self.files_list:
+                self.result_text.insert(END, file + '\n' * 3)
+                has_results = False
+
+                with open(file, encoding="utf8") as f:
+                    line_num = 1
+                    for line in f:
+                        if all([word in line.lower()
+                                for word in entered_words_list]):
+                            line_result = f'Line #{line_num}:\n{line}' + '\n' * 2
+                            self.result_text.insert(END, line_result)
+                            has_results = True
+                        line_num += 1
+
+                if not has_results:
+                    self.result_text.insert(END, NO_FILE_RESULTS_MESSAGE)
+
+            for i, word_str in enumerate(entered_str_list):
+                word_regex = r'(?i){}'.format(entered_words_list[i])
+                if word_str[-1] == '*':
+                    word_regex += r'\w*'
+                else:
+                    word_regex += ' '
+                self.result_text.highlight_pattern(word_regex, WORDS_COLORS[i])
 
 
 # Starting the application by drawing the new window
